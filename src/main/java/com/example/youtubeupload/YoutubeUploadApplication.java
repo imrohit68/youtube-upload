e770a893f78ea04e7a41ccc74b1addeda49a96df
Rohit Singh <sirohit328@gmail.com>
2024-04-09 14:12:39 +0530

    API documentation heading changed

--- src/main/java/com/example/youtubeupload/YoutubeUploadApplication.java
+++ src/main/java/com/example/youtubeupload/YoutubeUploadApplication.java
@@ -1,9 +1,23 @@
 package com.example.youtubeupload;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API DOCUMENTATION FOR YOUTUBE-UPLOAD APPLICATION",
+                description = "YOUTUBE-UPLOAD CHANNEL MANAGEMENT DOCUMENTATION ",
+                version = "v1",
+                contact = @Contact(
+                        name = " Rohit Singh",
+                        email = "sirohit328@gmail.com"
+                )
+        )
+)
 public class YoutubeUploadApplication {
 
     public static void main(String[] args) {
